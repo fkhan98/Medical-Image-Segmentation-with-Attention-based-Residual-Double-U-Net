46301acd993ce87c72414d5aeb173c0fc91f7c73
unknown <fahimshahriar52@iut-dhaka.edu>
2022-02-22 21:46:33 +0600

    Two attention gates placed

---
+++ model.py
@@ -0,0 +1,229 @@
+
+import tensorflow as tf
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications import *
+import tensorflow.keras.backend as K
+
+def expend_as(tensor, rep):
+     return Lambda(lambda x, repnum: K.repeat_elements(x, repnum, axis=3),
+                          arguments={'repnum': rep})(tensor)
+
+def gating_signal(input, out_size, batch_norm=False):
+    """
+    resize the down layer feature map into the same dimension as the up layer feature map
+    using 1x1 conv
+    :param input:   down-dim feature map
+    :param out_size:output channel number
+    :return: the gating feature map with the same dimension of the up layer feature map
+    """
+    x = Conv2D(out_size, (1, 1), padding='same')(input)
+    if batch_norm:
+        x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    return x
+
+def attention_block(x, gating, inter_shape):
+    shape_x = K.int_shape(x)
+    shape_g = K.int_shape(gating)
+
+    theta_x = Conv2D(inter_shape, (2, 2), strides=(2, 2), padding='same')(x)  # 16
+    shape_theta_x = K.int_shape(theta_x)
+
+    phi_g = Conv2D(inter_shape, (1, 1), padding='same')(gating)
+    upsample_g = Conv2DTranspose(inter_shape, (3, 3),
+                                 strides=(shape_theta_x[1] // shape_g[1], shape_theta_x[2] // shape_g[2]),
+                                 padding='same')(phi_g)  # 16
+
+    concat_xg = add([upsample_g, theta_x])
+    act_xg = Activation('relu')(concat_xg)
+    psi = Conv2D(1, (1, 1), padding='same')(act_xg)
+    sigmoid_xg = Activation('sigmoid')(psi)
+    shape_sigmoid = K.int_shape(sigmoid_xg)
+    upsample_psi = UpSampling2D(size=(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2]))(sigmoid_xg)  # 32
+
+    upsample_psi = expend_as(upsample_psi, shape_x[3])
+
+    y = multiply([upsample_psi, x])
+
+    result = Conv2D(shape_x[3], (1, 1), padding='same')(y)
+    result_bn = BatchNormalization()(result)
+    return result_bn
+
+def squeeze_excite_block(inputs, ratio=8):
+    init = inputs
+    channel_axis = -1
+    filters = init.shape[channel_axis]
+    se_shape = (1, 1, filters)
+
+    se = GlobalAveragePooling2D()(init)
+    se = Reshape(se_shape)(se)
+    se = Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
+    se = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
+
+    x = Multiply()([init, se])
+    return x
+
+def conv_block(inputs, filters):
+    x = inputs
+
+    x = Conv2D(filters, (3, 3), padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters, (3, 3), padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = squeeze_excite_block(x)
+
+    return x
+
+def encoder1(inputs):
+    skip_connections = []
+
+    model = VGG19(include_top=False, weights='imagenet', input_tensor=inputs)
+    names = ["block1_conv2", "block2_conv2", "block3_conv4", "block4_conv4"]
+    for name in names:
+        skip_connections.append(model.get_layer(name).output)
+
+    output = model.get_layer("block5_conv4").output
+    return output, skip_connections
+
+def decoder1(inputs, skip_connections):
+    num_filters = [256, 128, 64, 32]
+    ##custom code##
+    channels = [512,256,128,64]
+    ##custom code##
+    skip_connections.reverse()
+    x = inputs
+    shape = x.shape
+
+    for i, f in enumerate(num_filters):
+        ##custom code##
+        gating = gating_signal(x, channels[i], True)
+        att = attention_block(skip_connections[i], gating, channels[i])
+        ##custom code##
+        x = Conv2DTranspose(shape[3], (2, 2), activation="relu", strides=(2, 2))(x)
+        #x = Concatenate()([x, skip_connections[i]])
+        ##custom code##
+        x = Concatenate()([x, att])
+        ##custom code##
+        x = conv_block(x, f)
+
+    return x
+
+# def encoder2(inputs):
+#     skip_connections = []
+#
+#     output = DenseNet121(include_top=False, weights='imagenet')(inputs)
+#     model = tf.keras.models.Model(inputs, output)
+#
+#     names = ["input_2", "conv1/relu", "pool2_conv", "pool3_conv"]
+#     for name in names:
+#         skip_connections.append(model.get_layer(name).output)
+#     output = model.get_layer("pool4_conv").output
+#
+#     return output, skip_connections
+
+def encoder2(inputs):
+    num_filters = [32, 64, 128, 256]
+    skip_connections = []
+    x = inputs
+
+    for i, f in enumerate(num_filters):
+        x = conv_block(x, f)
+        skip_connections.append(x)
+        x = MaxPool2D((2, 2))(x)
+
+    return x, skip_connections
+
+def decoder2(inputs, skip_1, skip_2):
+    num_filters = [256, 128, 64, 32]
+    ##custom code##{
+    channels = [512,256,128,64]
+    ##custom code##}
+    skip_2.reverse()
+    x = inputs
+    
+
+    for i, f in enumerate(num_filters):
+        ##custom code##{
+        gating_enc_2 = gating_signal(x, num_filters[i], True)
+        att_enc_2 = attention_block(skip_2[i], gating_enc_2, num_filters[i])
+        ##custom code##}
+
+        x = UpSampling2D((2, 2), interpolation='bilinear')(x)
+        #x = Concatenate()([x, skip_1[i], skip_2[i]])
+        ##custom code##{
+        x = Concatenate()([x, skip_1[i], att_enc_2])
+        ##custom code##}
+        x = conv_block(x, f)
+
+    return x
+
+def output_block(inputs):
+    x = Conv2D(1, (1, 1), padding="same")(inputs)
+    x = Activation('sigmoid')(x)
+    return x
+
+def Upsample(tensor, size):
+    """Bilinear upsampling"""
+    def _upsample(x, size):
+        return tf.image.resize(images=x, size=size)
+    return Lambda(lambda x: _upsample(x, size), output_shape=size)(tensor)
+
+def ASPP(x, filter):
+    shape = x.shape
+
+    y1 = AveragePooling2D(pool_size=(shape[1], shape[2]))(x)
+    y1 = Conv2D(filter, 1, padding="same")(y1)
+    y1 = BatchNormalization()(y1)
+    y1 = Activation("relu")(y1)
+    y1 = UpSampling2D((shape[1], shape[2]), interpolation='bilinear')(y1)
+
+    y2 = Conv2D(filter, 1, dilation_rate=1, padding="same", use_bias=False)(x)
+    y2 = BatchNormalization()(y2)
+    y2 = Activation("relu")(y2)
+
+    y3 = Conv2D(filter, 3, dilation_rate=6, padding="same", use_bias=False)(x)
+    y3 = BatchNormalization()(y3)
+    y3 = Activation("relu")(y3)
+
+    y4 = Conv2D(filter, 3, dilation_rate=12, padding="same", use_bias=False)(x)
+    y4 = BatchNormalization()(y4)
+    y4 = Activation("relu")(y4)
+
+    y5 = Conv2D(filter, 3, dilation_rate=18, padding="same", use_bias=False)(x)
+    y5 = BatchNormalization()(y5)
+    y5 = Activation("relu")(y5)
+
+    y = Concatenate()([y1, y2, y3, y4, y5])
+
+    y = Conv2D(filter, 1, dilation_rate=1, padding="same", use_bias=False)(y)
+    y = BatchNormalization()(y)
+    y = Activation("relu")(y)
+
+    return y
+
+def build_model(shape):
+    inputs = Input(shape)
+    x, skip_1 = encoder1(inputs)
+    x = ASPP(x, 64)
+    x = decoder1(x, skip_1)
+    outputs1 = output_block(x)
+
+    x = inputs * outputs1
+
+    x, skip_2 = encoder2(x)
+    x = ASPP(x, 64)
+    x = decoder2(x, skip_1, skip_2)
+    outputs2 = output_block(x)
+    outputs = Concatenate()([outputs1, outputs2])
+
+    model = Model(inputs, outputs)
+    return model
+
+if __name__ == "__main__":
+    model = build_model((192, 256, 3))
+    model.summary()
